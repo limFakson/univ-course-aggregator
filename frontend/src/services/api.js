fae872344883894fd4254f6d53058b01456f1835
Joshua Opeyemi <fakeyejoshua2005@gmail.com>
2025-10-12 20:11:15 +0100

    update: service api url from localhost url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8001/api",
+  baseURL: "/api",
 });
 
 export const getCourses = (params = {}) => api.get("/courses", { params });
